4d6004c02d2101a686dcc62e2a1de16d29dfd5d9
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-09 16:10:59 +0000

    corrected boilerplate

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -14,6 +14,8 @@ class BoardsController < ApplicationController
 
     @the_board = matching_boards.at(0)
 
+    @list_of_posts = Post.where({ :board_id => the_id }).order({ :expires_on => :asc })
+
     render({ :template => "boards/show" })
   end
 
